cff4808e4e6c2ec0b2bf2ea34b94c333980bfc40
tmartin8080 <troy@devato.com>
2023-01-27 17:34:04 -0500

    Add comments and constraints

--- lib/leet/validate_binary_search_tree.ex
+++ lib/leet/validate_binary_search_tree.ex
@@ -7,16 +7,22 @@ defmodule TreeNode do
   defstruct val: 0, left: nil, right: nil
 end
 
-# def validate(node, low=-math.inf, high=math.inf):
-# Empty trees are valid BSTs.
-# if not node: return True
-# The current node's value must be between low and high.
-# if node.val <= low or node.val >= high:  return False
-
-# The left and right subtree must also be valid.
-# return (validate(node.right, node.val, high) and  validate(node.left, low, node.val))
-
 defmodule BST do
+  @moduledoc """
+  Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+  A valid BST is defined as follows:
+
+  1. The left subtree of a node contains only nodes with keys less than the node's key.
+  2. The right subtree of a node contains only nodes with keys greater than the node's key.
+  3. Both the left and right subtrees must also be binary search trees.
+
+  ## Constraints:
+
+  1. The number of nodes in the tree is in the range [1, 104].
+  2. -2^31 <= Node.val <= 2^31 - 1
+
+  """
   @min :math.pow(-2, 31)
   @max :math.pow(2, 31) - 1
 
