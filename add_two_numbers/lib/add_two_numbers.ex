527e4c41c5a8d03a04e104c28e57c30ef1e1dd35
tmartin8080 <troy@devato.com>
2023-01-28 12:47:15 -0500

    Add repo

---
+++ add_two_numbers/lib/add_two_numbers.ex
@@ -0,0 +1,18 @@
+defmodule AddTwoNumbers do
+  @moduledoc """
+  Documentation for `AddTwoNumbers`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AddTwoNumbers.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ add_two_numbers/test/add_two_numbers_test.exs
@@ -0,0 +1,8 @@
+defmodule AddTwoNumbersTest do
+  use ExUnit.Case
+  doctest AddTwoNumbers
+
+  test "greets the world" do
+    assert AddTwoNumbers.hello() == :world
+  end
+end
