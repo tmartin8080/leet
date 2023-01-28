3f8cbefe10f273200f00e44dc353492dab6091dc
tmartin8080 <troy@devato.com>
2023-01-28 13:05:11 -0500

    Reverse and split

--- add_two_numbers/lib/add_two_numbers.ex
+++ add_two_numbers/lib/add_two_numbers.ex
@@ -1,18 +1,36 @@
 defmodule AddTwoNumbers do
-  @moduledoc """
-  Documentation for `AddTwoNumbers`.
-  """
+  @spec add_two_numbers(l1 :: ListNode.t() | nil, l2 :: ListNode.t() | nil) :: ListNode.t() | nil
+  def add_two_numbers(l1, l2) do
+    n1 = list_to_value(l1)
+    n2 = list_to_value(l2)
 
-  @doc """
-  Hello world.
+    (n1 + n2)
+    |> to_string()
+    |> String.split("", trim: true)
+    |> Enum.reverse()
+    |> Enum.map(fn int_string ->
+      Integer.parse(int_string)
+      |> case do
+        {int, _} ->
+          int
 
-  ## Examples
+        _ ->
+          0
+      end
+    end)
+  end
 
-      iex> AddTwoNumbers.hello()
-      :world
+  defp list_to_value(list) do
+    list
+    |> Enum.with_index()
+    |> Enum.reduce(0, fn
+      {value, 0}, acc ->
+        acc + value
 
-  """
-  def hello do
-    :world
+      {value, index}, acc ->
+        multiplier = :math.pow(10, index)
+        acc + value * multiplier
+    end)
+    |> trunc()
   end
 end
--- add_two_numbers/test/add_two_numbers_test.exs
+++ add_two_numbers/test/add_two_numbers_test.exs
@@ -1,8 +1,20 @@
 defmodule AddTwoNumbersTest do
   use ExUnit.Case
-  doctest AddTwoNumbers
+  import AddTwoNumbers
 
-  test "greets the world" do
-    assert AddTwoNumbers.hello() == :world
+  test "adds 3-digit lists" do
+    l1 = [2, 4, 3]
+    l2 = [5, 6, 4]
+    assert add_two_numbers(l1, l2) == [7, 0, 8]
+  end
+
+  test "adds zero lists" do
+    assert add_two_numbers([0], [0]) == [0]
+  end
+
+  test "adds 9s lists" do
+    l1 = [9, 9, 9, 9, 9, 9, 9]
+    l2 = [9, 9, 9, 9]
+    assert add_two_numbers(l1, l2) == [8, 9, 9, 9, 0, 0, 0, 1]
   end
 end
