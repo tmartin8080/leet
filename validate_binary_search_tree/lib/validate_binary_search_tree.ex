bae735fb42f70b12e3dce65bcc67219ec442d92c
tmartin8080 <troy@devato.com>
2023-01-28 06:48:02 -0500

    Switch to folders

---
+++ validate_binary_search_tree/lib/validate_binary_search_tree.ex
@@ -0,0 +1,35 @@
+defmodule ValidateBinarySearchTree do
+  @moduledoc """
+  Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+  A valid BST is defined as follows:
+
+  1. The left subtree of a node contains only nodes with keys less than the node's key.
+  2. The right subtree of a node contains only nodes with keys greater than the node's key.
+  3. Both the left and right subtrees must also be binary search trees.
+
+  ## Constraints:
+
+  1. The number of nodes in the tree is in the range [1, 104].
+  2. -2^31 <= Node.val <= 2^31 - 1
+
+  """
+  @min :math.pow(-2, 31)
+  @max :math.pow(2, 31) - 1
+
+  @spec is_valid_bst(root :: TreeNode.t() | nil) :: boolean
+  def is_valid_bst(root) do
+    validate(root, trunc(@min), trunc(@max))
+  end
+
+  # Empty trees are valid BSTs.
+  defp validate(nil, _low, _high), do: true
+
+  # The current node's value must be between low and high.
+  defp validate(%{val: val}, low, high) when val <= low or val >= high, do: false
+
+  # The left and right subtree must also be valid.
+  defp validate(node, low, high) do
+    validate(node.right, node.val, high) and validate(node.left, low, node.val)
+  end
+end
