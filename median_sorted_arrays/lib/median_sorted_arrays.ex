67843ea30a3efd46912427605a464f123874b6d2
tmartin8080 <troy@devato.com>
2023-02-19 09:23:25 -0500

    Update

--- median_sorted_arrays/lib/median_sorted_arrays.ex
+++ median_sorted_arrays/lib/median_sorted_arrays.ex
@@ -17,7 +17,7 @@ defmodule MedianSortedArrays do
   @spec find_median_sorted_arrays(nums1 :: [integer], nums2 :: [integer]) :: float
   def find_median_sorted_arrays(nums1, nums2) do
     {{a1, m}, {a2, n}} = shortest_first(nums1, nums2)
-    range = {0, m - 1}
+    range = {0, max(m, 0)}
     find_median(range, {a1, m}, {a2, n})
   end
 
@@ -36,8 +36,6 @@ defmodule MedianSortedArrays do
     a2_min_right =
       if a2_partition == n, do: @positive_infinity, else: :array.get(a2_partition, a2)
 
-    dbg()
-
     cond do
       a1_max_left <= a2_min_right and a2_max_left <= a1_min_right ->
         if Integer.mod(m + n, 2) == 0 do
@@ -47,7 +45,7 @@ defmodule MedianSortedArrays do
         end
 
       a1_max_left > a2_min_right ->
-        new_high = a2_partition - 1
+        new_high = a1_partition - 1
         find_median({low, new_high}, {a1, m}, {a2, n})
 
       true ->
@@ -58,21 +56,21 @@ defmodule MedianSortedArrays do
 
   # convert to actual arrays and sort by ascending length
   defp shortest_first(nums1, nums2) do
-    nums1 = :array.from_list(nums1)
-    nums2 = :array.from_list(nums2)
-    nums1_length = :array.size(nums1)
-    nums2_length = :array.size(nums2)
+    a1 = :array.from_list(nums1)
+    a2 = :array.from_list(nums2)
+    a1_length = :array.size(a1)
+    a2_length = :array.size(a2)
 
-    if nums1_length <= nums2_length do
-      {{nums1, nums1_length}, {nums2, nums2_length}}
+    if a1_length <= a2_length do
+      {{a1, a1_length}, {a2, a2_length}}
     else
-      {{nums2, nums2_length}, {nums1, nums1_length}}
+      {{a2, a2_length}, {a1, a1_length}}
     end
   end
 
   defp partition_arrays({low, high}, m, n) do
-    partition_m = round((low + high) / 2)
-    partition_n = round((m + n) / 2 - partition_m)
-    {max(partition_m, 0), max(partition_n, 0)}
+    a1_partition = round((low + high) / 2)
+    a2_partition = round((m + n) / 2 - a1_partition)
+    {max(a1_partition, 0), max(a2_partition, 0)}
   end
 end
--- median_sorted_arrays/test/median_sorted_arrays_test.exs
+++ median_sorted_arrays/test/median_sorted_arrays_test.exs
@@ -5,8 +5,9 @@ defmodule MedianSortedArraysTest do
   describe "find_median_sorted_arrays/2" do
     test "general cases" do
       assert find_median_sorted_arrays([1, 3], [2]) == 2
-      assert find_median_sorted_arrays([1, 2], [3, 4]) == 2.5
       assert find_median_sorted_arrays([2], []) == 2
+      assert find_median_sorted_arrays([], [2, 3]) == 2.5
+      assert find_median_sorted_arrays([100_001], [100_000]) == 100_000.5
     end
 
     test "one empty list case" do
@@ -22,11 +23,8 @@ defmodule MedianSortedArraysTest do
     assert -2 > :math.pow(10, 6) * -1
   end
 
-  test "sup with these numberss" do
-    assert find_median_sorted_arrays([100_001], [100_000]) == 100_000.5
-  end
-
   test "another fun one" do
-    assert find_median_sorted_arrays([], [2, 3]) == 2.5
+    assert find_median_sorted_arrays([1, 2], [3, 4]) == 2.5
+    # assert find_median_sorted_arrays([4, 5], [1, 2, 3]) == 100_000.5
   end
 end
