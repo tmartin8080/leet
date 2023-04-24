a4aa0369004f5f738a313ecf91fc51fb097794d8
tmartin8080 <troy@devato.com>
2023-04-24 10:59:53 -0400

    Save scratch

--- scratch/lib/quad.ex
+++ scratch/lib/quad.ex
@@ -2,8 +2,17 @@ defmodule Quad do
   def has_duplicate_value?(data) do
     data
     |> Enum.with_index()
-    |> Enum.map(fn {n, index} ->
-      Enum.find_index(data, n)
+    |> Enum.flat_map(fn {val1, index1} ->
+      data
+      |> Enum.with_index()
+      |> Enum.map(fn {val2, index2} ->
+        if val1 == val2 and index1 != index2 do
+          true
+        else
+          false
+        end
+      end)
     end)
+    |> Enum.any?(&(&1 == true))
   end
 end
